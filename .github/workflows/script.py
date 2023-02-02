beead62246941604d252cc998e05a677e7529b24
Diego Antunes <diego.costa@ciag.org.br>
2023-02-02 16:34:56 -0300

    up

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -20,7 +20,7 @@ def get_csv_column(filename, column):
             yield row[column]
 
 
-for i in range(9000, 45000):
+for i in range(1, 45000):
     email = list(get_csv_column(PATH, 0))[i]
 
     # name = list(get_csv_column(PATH, 1))[i]
