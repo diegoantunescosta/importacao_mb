b8e48a7c1288e7d2e8b89617e0a9ee1a2d349707
Diego Antunes <diego.costa@ciag.org.br>
2023-02-02 14:56:31 -0300

    up

---
+++ script.py
@@ -0,0 +1,41 @@
+import csv
+from datetime import datetime
+import requests
+import time
+import pandas as pd
+!pip install pandas
+
+
+
+API_ENDPOINT = "https://member.mailingboss.com/integration/index.php/lists/subscribers/create/110895:d9db4a4176ee8a69750ba441cfe97a5d"
+LIST_UID = '62e7d1065030b'
+PATH = 'importacao.csv'
+results = pd.read_csv(PATH)
+ROWS = len(results) + 1
+now = datetime.now()
+
+
+def get_csv_column(filename, column):
+    with open(filename) as stream:
+        reader = csv.reader(stream)
+        for row in reader:
+            yield row[column]
+
+
+for i in range(1, ROWS):
+    email = list(get_csv_column(PATH, 0))[i]
+    # name = list(get_csv_column(PATH, 1))[i]
+    status = 'confirmed'
+    data = {
+        'list_uid': LIST_UID,
+        'email': email,
+        #    'name': name,
+        'status': status
+    }
+    time.sleep(1)
+    r = requests.post(url=API_ENDPOINT, data=data)
+    pastebin_url = r.text
+    print(
+        "Tentativa de cadastro no horário: {} no email: {} resposta do Json: {}"
+        .format(now, email, pastebin_url))
+print("IMPORTADO COM SUCESSO !")
